2c201626b1eeacf136dbc6bc61487565323e9636
SantiAS <99286126+santistockcode@users.noreply.github.com>
2024-09-11 18:51:36 +0200

    End reading chapter 1

--- get_next_line.h
+++ get_next_line.h
@@ -1,5 +1,10 @@
 #ifndef GNL_H
 # define GNL_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 3
+# endif
+
 # include <stddef.h>
 # include <stdlib.h>
 # include <limits.h>
--- main.c
+++ main.c
@@ -3,19 +3,31 @@
 #include <fcntl.h>
 
 
+// me quedo en el chapter 2
+
 int main(void)
 {
   int    fd;
   char  *next_line;
   int  count;
 
-  count = 0;
-  fd = open("example.txt", O_RDONLY);
-  next_line = get_next_line(fd);
-  count++;
-  printf("[%d]:%s\n", count, next_line); //count is to show you the line numbers
-  next_line = NULL;
-
-  close(fd);
-  return (0);
+    count = 0;
+    fd = open("example.txt", O_RDONLY);
+    if (fd == -1)
+    {
+        printf("Error opening da thing");
+        return (1);
+    }
+    while (1)
+    {
+        next_line = get_next_line(fd);
+        if (next_line == NULL)
+            break ;
+        count++;
+        printf("[%d]:%s\n", count, next_line); //count is to show you the line numbers
+        free(next_line);
+        next_line = NULL;
+    }
+    close(fd);
+    return (0);
 }
